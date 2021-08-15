5c1d175c2df8232e4ac1334fd29b2703ed0fbc9d
Md zoynul abedin <33788041+zoynulabedin@users.noreply.github.com>
2021-08-15 16:17:45 +0600

    update

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>oop crud project</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+</head>
+<body>
+    
+<div class="users">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 bg-dark">
+                <form action="">
+                <label for="name" class="col-sm-2 col-form-label">Name</label>
+                <input class="form-control" id="name" name="name" type="text">
+                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                <input class="form-control" name="email" type="email">
+                <label for="pass" class="col-sm-2 col-form-label">Email</label>
+                <input id="pass" class="form-control" type="password">
+                <input type="submit" value="Recored" name="recored">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
