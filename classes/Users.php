c2426791ff1da7abbb3a6bef95baa29c1cba6a5e
Md zoynul abedin <trustcodernet@gmail.com>
2021-08-16 21:22:26 +0600

    oop crud

--- classes/Users.php
+++ classes/Users.php
@@ -1,28 +1,25 @@
-<?php 
+<?php
 
 class User{
 
     public function __construct(){
-       $localhost = "localhost:3325:4306";
-       $user  = "root";
-       $pass = "";
-       $db_name = "oop";
+        $host = "localhost:3325:4306";
+        $user = "root";
+        $pass = "";
+        $db = "oop";
 
-
-     $this->link = mysqli_connect($localhost,$user,$pass,$db_name);
-
-
-
+        $this->link = mysqli_connect($host,$user,$pass,$db);
     }
 
-    public function insert_data($data){
-       $name = $data['name'];
-       $email = $data['email'];
-        $pass = $data['password'];
-
-        $query = "INSERT INTO 'users'('name', 'email', 'password') VALUES ('$name','$email','$pass')";
-
-      mysqli_query($this->link,$query);
+    public function insert_user($data){
+        $name = $data['name'];
+        $email = $data['email'];
+        $password = $data['password'];
+        $query = "INSERT INTO `users`(`name`, `email`, `password`) VALUES ('$name','$email','$password')";
+        mysqli_query($this->link,$query);
     }
 
+    public function allusers(){
+      return  mysqli_query($this->link,"SELECT * FROM 'users'");
+    }
 }
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 require_once "classes/Users.php";
 $user = new User();
+
 if(isset($_POST['recored'])){
-   $user->insert_data($_POST);
+    $user->insert_user($_POST);
 }
+ $all_users = $user->allusers();
+
 ?>
 
 <!DOCTYPE html>
@@ -17,20 +20,55 @@ if(isset($_POST['recored'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 </head>
 <body>
-    
+
 <div class="users mt-5">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 bg-dark p-5 shadow-lg rounded">
                 <form action="" method="POST">
-                <label for="name" class="col-sm-2 col-form-label text-light">Name</label>
-                <input class="form-control" id="name" name="name" placeholder="Name" type="text">
-                <label for="email" class="col-sm-2 col-form-label text-light">Email</label>
-                <input class="form-control" name="email" placeholder="Email" type="email">
-                <label for="pass" class="col-sm-2 col-form-label text-light">Email</label>
-                <input id="pass" name="password" class="form-control" placeholder="Password" type="password">
-                <input type="submit" class="btn btn-info mt-5" value="Recored" name="recored">
+                    <label for="name" class="col-sm-2 col-form-label text-light">Name</label>
+                    <input class="form-control" id="name" name="name" placeholder="Name" type="text">
+                    <label for="email" class="col-sm-2 col-form-label text-light">Email</label>
+                    <input class="form-control" name="email" placeholder="Email" type="email">
+                    <label for="pass" class="col-sm-2 col-form-label text-light">Email</label>
+                    <input id="pass" name="password" class="form-control" placeholder="Password" type="password">
+                    <input type="submit" class="btn btn-info mt-5" value="Recored" name="recored">
                 </form>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+<div class="all-users">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <table class="table" border="1">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php $row = mysqli_fetch_assoc($all_users);
+                    foreach( $row as $single){
+
+                    ?>
+                        <tr>
+
+                            <th scope="row">1</th>
+                            <td><?php echo $single['name'];?></td>
+                            <td><?php echo $single['email'];?></td>
+                            <td><a href="#">Edit</a></td>
+                            <td><a href="#">Delete</a></td>
+                        </tr>
+                    <?php };?>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
